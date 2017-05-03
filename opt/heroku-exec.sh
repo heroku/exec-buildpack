41ceae6344e14ae912eedc9dae07b25b9109914f
Joe Kutner <jpkutner@gmail.com>
2017-05-03 14:58:24 -0500

    Added staging config

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -2,6 +2,7 @@
 
 if [ -n "$HEROKU_PRIVATE_IP" ] || [[ "$DYNO" != run\.* ]]; then
   if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
+    export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/heroku-exec\./heroku-exec-staging\./)"
     source <(curl --fail --max-time 5 -sSL "$HEROKU_EXEC_URL")
   fi
 fi
