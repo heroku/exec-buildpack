17ee22763c88f1367be9fae798fbdc715a07d764
BuildpackTester <jpkutner@gmail.com>
2017-09-28 04:54:46 -0500

    Added guard for non-space one-off dynos

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
-  source <(curl --fail -sSL "$HEROKU_EXEC_URL")
+if [ -n "$HEROKU_PRIVATE_IP" ] || [[ "$DYNO" != run\.* ]]; then
+  if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
+    source <(curl --fail -sSL "$HEROKU_EXEC_URL")
+  fi
 fi
