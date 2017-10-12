5dfd6a12fbd2c07812d7985572b50c234d70979c
BuildpackTester <jpkutner@gmail.com>
2017-10-12 08:15:41 -0500

    Added logging

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -2,6 +2,7 @@
 
 if [ -n "$HEROKU_PRIVATE_IP" ] || [[ "$DYNO" != run\.* ]]; then
   if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
+    echo "[heroku-exec] Using staging buildpack"
     export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/heroku-exec\.herokuapp/heroku-exec-staging\.herokuapp/)"
     sshd_pid="$(ps -C sshd -o pid=)"
     ssh_pid="$(ps -C ssh -o pid=)"
