7381589a71428a13329a3eab1dff19e1514dea6a
BuildpackTester <jpkutner@gmail.com>
2017-11-21 12:43:35 -0600

    Add --max-time to curl request

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -2,6 +2,6 @@
 
 if [ -n "$HEROKU_PRIVATE_IP" ] || [[ "$DYNO" != run\.* ]]; then
   if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
-    source <(curl --fail -sSL "$HEROKU_EXEC_URL")
+    source <(curl --fail --max-time 5 -sSL "$HEROKU_EXEC_URL")
   fi
 fi
