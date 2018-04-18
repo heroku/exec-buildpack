560ba464699b5792465894fcda8a712ab754effc
Joe Kutner <jpkutner@gmail.com>
2018-04-18 10:26:36 -0500

    Changed staging URL to exec-manager-staging

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -3,7 +3,7 @@
 if [ -n "$HEROKU_PRIVATE_IP" ] || [[ "$DYNO" != run\.* ]]; then
   if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
     echo "[heroku-exec] Using staging buildpack"
-    export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/heroku-exec\.herokuapp/heroku-exec-staging\.herokuapp/)"
+    export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/exec-manager\.heroku/exec-manager-staging\.herokai/)"
     sshd_pid="$(ps -C sshd -o pid=)"
     ssh_pid="$(ps -C ssh -o pid=)"
     [ -n "$sshd_pid" ] && kill -9 "$sshd_pid"
