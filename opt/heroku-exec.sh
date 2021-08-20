4d4e71b1ec2db680cd8e578c144728cdbbb588c0
Terence Lee <hone02@gmail.com>
2021-08-20 13:13:56 -0700

    update regex to account for beginning anchor and periods

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -n "$HEROKU_PRIVATE_IP" ] || ! [[ "$DYNO" =~ run\.*|release\.* ]]; then
+if [ -n "$HEROKU_PRIVATE_IP" ] || ! [[ "$DYNO" =~ ^(run\..*|release\..*) ]]; then
   if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
     source <(curl --fail --max-time 5 -sSL "$HEROKU_EXEC_URL")
   fi
