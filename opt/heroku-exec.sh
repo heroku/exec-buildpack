2e24ee9641db74efb3d93d6cda9badda26fbe4b4
Benjamin Leong <benri@users.noreply.github.com>
2022-11-16 12:01:13 -0800

    Update heroku-exec.sh

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -n "$HEROKU_PRIVATE_IP" ] || [[ "$DYNO" != run\.* ]]; then
+if [ -n "$HEROKU_PRIVATE_IP" ] || ! [[ "$DYNO" =~ ^(run\..*|release\..*) ]]; then
   if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
     echo "[heroku-exec] Using staging buildpack"
     export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/exec-manager\.heroku/exec-manager-staging\.herokai/)"
-    sshd_pid="$(ps -C sshd -o pid=)"
-    ssh_pid="$(ps -C ssh -o pid=)"
-    [ -n "$sshd_pid" ] && kill -9 "$sshd_pid"
-    [ -n "$ssh_pid" ] && kill -9 "$ssh_pid"
     source <(curl --fail --max-time 5 -sSL "$HEROKU_EXEC_URL")
   fi
 fi
