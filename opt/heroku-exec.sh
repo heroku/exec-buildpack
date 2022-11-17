c2e5e6ef7d0395de3f290df8246a1831a1732263
Benjamin Leong <benri@users.noreply.github.com>
2022-11-17 13:04:40 -0800

    Update staging branch with master (#6)
    * don't run heroku exec during release phase (#3)
    
    * update regex to account for beginning anchor and periods (#4)
    
    * Updated/Added CODEOWNERS with ECCN
    
    Co-authored-by: Terence Lee <hone02@gmail.com>
    Co-authored-by: svc-scm <48930134+svc-scm@users.noreply.github.com>

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -n "$HEROKU_PRIVATE_IP" ] || [[ "$DYNO" != run\.* ]]; then
+if [ -n "$HEROKU_PRIVATE_IP" ] || ! [[ "$DYNO" =~ ^(run\..*|release\..*) ]]; then
   if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
     echo "[heroku-exec] Using staging buildpack"
     export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/exec-manager\.heroku/exec-manager-staging\.herokai/)"
